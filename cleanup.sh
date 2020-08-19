13fa9adc4808dbdf8fe605a8ec5a976d2a105787
Juliane Waack <jkwaack@gmail.com>
2020-08-19 16:26:04 +0200

    add files with test configurations for easier testing

--- cleanup.sh
+++ cleanup.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-NAME=${1?Error: no folder name given}
+NAME=${1?Error: no log name given}
 
 mkdir "data/tests"
-mkdir "data/tests/$NAME"
-mv "data/aknng.graph" "data/tests/$NAME/akkng.graph"
-mv "data/nsg.graph" "data/tests/$NAME/nsg.graph"
-mv "data/dNSG.log" "data/tests/$NAME/dNSG.log"
+mv "data/dNSG.log" "data/tests/${NAME}.log"
---
+++ startTest.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+NAME=${1?Error: no config file given}
+TEST=${2?Error: no test file given}
+
+#clean up old logs if still there
+rm "data/dNSG.log"
+# get configuration for this test
+testFile="testConfigurations/${TEST}"
+# get current nodes configuration and combine
+cat $testFile $NAME > testConfigurations.txt
+#start node
+java -Xmx28g -XX:+UseG1GC -Dconfig.file="testConfigurations.txt" -jar target/scala-2.13/dNSG-assembly-0.1.0-SNAPSHOT.jar
+# move log
+./cleanup.sh "$TEST"
